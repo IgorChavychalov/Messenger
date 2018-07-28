73d0d04f7d3b34588d7bbad339c90d3e2a6451a3
IgorChavychalov <Protor_fenix@mail.ru>
2018-07-28 14:35:10 +0300

    Создан модуль - для логга сервера

---
+++ log/server_log_config.py
@@ -0,0 +1,29 @@
+import logging.handlers
+import os
+
+# собственные модули
+from settings import ENCODING
+
+# Папка где лежит этот файл
+FOLDER = os.path.dirname(os.path.abspath(__file__)) + '\\files'
+
+# Пусть до серверного лога
+PATH = os.path.join(FOLDER, 'server.log')
+
+# Создание логгера
+server_logger = logging.getLogger('server')
+# Форматирование
+formatter = logging.\
+    Formatter(u'%(levelname)s  [%(asctime)s]  %(filename)s  [%(lineno)d]  %(funcName)s:  %(message)s')
+
+# Создаем обработчик с ротацией файлов по дням
+server_handler = logging.handlers.TimedRotatingFileHandler(PATH, when='D', encoding=ENCODING)
+# Задаём формат
+server_handler.setFormatter(formatter)
+
+# Связываем логгер с обработчиком
+server_logger.addHandler(server_handler)
+# устанавливаем уровень логгера
+server_logger.setLevel(logging.DEBUG)
+
+
