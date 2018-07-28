f922e0e6f6e7947fc81af4ce6bd90e1519245aac
IgorChavychalov <Protor_fenix@mail.ru>
2018-07-28 14:32:42 +0300

    Создан модуль - ошибки для запросов к базе

---
+++ base/errors.py
@@ -0,0 +1,51 @@
+class LoginIsUsed(Exception):
+    def __init__(self, login):
+        self.login = login
+
+    def __str__(self):
+        return f'login {self.login} is used'
+
+
+class LoginIsNotInTable(Exception):
+    # LoginDoseNotTable
+    def __init__(self, login):
+        self.login = login
+
+    def __str__(self):
+        return f'{self.login} is not in Users table'
+
+
+class IdIsNotInTable(Exception):
+    def __init__(self, login):
+        self.login = login
+
+    def __str__(self):
+        return f'{self.login}ID is not in History table'
+
+
+class LoginHasNoContacts(Exception):
+    def __init__(self, login):
+        self.login = login
+
+    def __str__(self):
+        return f'{self.login} has no contacts'
+
+
+class UsersAreNotFrends(Exception):
+    def __init__(self, user, frend):
+        self.user = user
+        self.frend = frend
+
+    def __str__(self):
+        return f'{self.user} and {self.frend} are not frends'
+
+
+class ContactIsExist(Exception):
+    def __init__(self, user, frend):
+        self.user = user
+        self.frend = frend
+
+    def __str__(self):
+        return f'<Contact {self.user} - {self.frend} is Exist'
+
+# АНГЛИЙСКИЙ ЗАСТРЕЛИЛСЯ
