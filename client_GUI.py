a297e89d81cb54123ec30f305e1a0a7a492d7987
IgorChavychalov <Protor_fenix@mail.ru>
2018-07-31 20:20:34 +0300

    Добавлен класс рессивер

--- client_GUI.py
+++ client_GUI.py
@@ -4,6 +4,37 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 import client
 import jim.JIM
 import jim.msg
+from client import Receiver
+from PyQt5.QtCore import QObject, pyqtSignal
+
+
+class GuiReciever(Receiver, QObject):
+    """GUI обработчик входящих сообщений"""
+    # мы его наследуюем от QObject чтобы работала модель сигнал слот
+    # можно и не наследовать, но тогда надо передавать объект в который мы будем сообщения выводить
+    # через сигнал слот более гибко т.к. мы можем обработать сигнал как хотим уже внутри gui
+    # событий (сигнал) что пришли данные
+    gotData = pyqtSignal(str)
+    # событие (сигнал) что прием окончен
+    finished = pyqtSignal(int)
+
+    def __init__(self, sock, request_queue):
+        # инициализируем как Receiver
+        Receiver.__init__(self, sock, request_queue)
+        # инициализируем как QObject
+        QObject.__init__(self)
+
+    def process_message(self, message):
+        """Обработка сообщения"""
+        # Генерируем сигнал (сообщаем, что произошло событие)
+        # В скобках передаем нужные нам данные
+        text = '{} >>> {}'.format(message.from_, message.message)
+        self.gotData.emit(text)
+
+    def poll(self):
+        super().poll()
+        # Когда обработка событий закончиться сообщаем об этом генерируем сигнал finished
+        self.finished.emit(0)
 
 
 class UserWindow(QMainWindow):
