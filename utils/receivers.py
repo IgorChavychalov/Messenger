4c995b4a2d9bd15d8519193219983469d86898cd
IgorChavychalov <Protor_fenix@mail.ru>
2018-08-03 16:48:00 +0300

    Добавлен класс рессивер для ГУИ

--- utils/receivers.py
+++ utils/receivers.py
@@ -1,4 +1,5 @@
 from .JIM import get_message
+from PyQt5.QtCore import QObject, pyqtSignal
 
 
 class Receiver:
@@ -49,3 +50,37 @@ class ConsoleReciever(Receiver):
                 print(f'\n[{t} {name} покинул чат]')
         except:
             pass
+
+class GuiReciever(Receiver, QObject):
+    """GUI обработчик входящих сообщений"""
+    # мы его наследуюем от QObject чтобы работала модель сигнал слот
+    # можно и не наследовать, но тогда надо передавать объект в который мы будем сообщения выводить
+    # через сигнал слот более гибко т.к. мы можем обработать сигнал как хотим уже внутри gui
+    # событий (сигнал) что пришли данные
+    gotData = pyqtSignal(str)
+    # событие (сигнал) что прием окончен
+    finished = pyqtSignal(int)
+
+    def __init__(self, sock, request_queue):
+        # инициализируем как Receiver
+        Receiver.__init__(self, sock, request_queue)
+        # инициализируем как QObject
+        QObject.__init__(self)
+
+    def process_message(self, message):
+        """Обработка сообщения"""
+        # Генерируем сигнал (сообщаем, что произошло событие)
+        # В скобках передаем нужные нам данные
+        action = message['action']
+        time_point = message['time']
+        t = time_point[11:]
+        if action == 'msg':
+            msg = message['message']
+            name = message['from']
+            text = (f'[{t} от {name}]: {msg}')
+            self.gotData.emit(text)
+
+    def poll(self):
+        super().poll()
+        # Когда обработка событий закончиться сообщаем об этом генерируем сигнал finished
+        self.finished.emit(0)
