4ace4f2aafed573f654fbe7ac16c6c853fb00e5e
IgorChavychalov <Protor_fenix@mail.ru>
2018-10-31 19:10:05 +0300

    Отключены консоли

--- start (GUI).py
+++ start (GUI).py
@@ -8,6 +8,8 @@ print('Сервер запущен')
 # ждем на всякий пожарный
 time.sleep(2)
 # запускаем 2 клиентов
-Popen('python client_GUI.py localhost 8888 Клиент', creationflags=CREATE_NEW_CONSOLE)
-Popen('python client_GUI.py localhost 8888 Пациент', creationflags=CREATE_NEW_CONSOLE)
+# Popen('python client_GUI.py localhost 8888 Клиент', creationflags=CREATE_NEW_CONSOLE)
+# Popen('python client_GUI.py localhost 8888 Пациент', creationflags=CREATE_NEW_CONSOLE)
+Popen('python client_GUI.py localhost 8888 Клиент')
+Popen('python client_GUI.py localhost 8888 Пациент')
 print('запущенно 2 клиента')
